e4bf7531eef4a00edaf0385d24dd2c86d0f6be06
Barak Saidoff <barak.saidoff@gmail.com>
2020-04-03 20:56:48 -0400

    viewport

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
         <header >
           <Nav></Nav>
           <CovidData></CovidData>
-          <meta name="viewport" content="width=device-width" ></meta>
         </header>
         <Switch>
           <Route exact path = "/"render={(routerProps)=> <Home {...routerProps}/> }/>
